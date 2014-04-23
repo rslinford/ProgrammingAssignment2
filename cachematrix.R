6a02690bf625ea5301eef35cd88de0f0cbef603b
Scott Linford <scott@linfords.com>
2014-04-23 16:19:17 -0400

    Initial commit to test git setup. No change of any substance.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+	## TODO
 }
 
-
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## Return a matrix that is the inverse of 'x'
 }
